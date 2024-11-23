fcbc180aa9f446df76bf880014806ce4b32c5ef4
Adailton Junior <adailton.jr@gmail.com>
2024-11-23 16:24:59 -0300

    melhorias na calsse de servico de autor.

--- src/main/java/com/aula/biblioteca/service/AutorService.java
+++ src/main/java/com/aula/biblioteca/service/AutorService.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,24 +17,23 @@ public class AutorService {
     private final AutorRepository autorRepository;
 
     public AutorDTO create(AutorDTO autorDTO) {
-        return new AutorDTO(autorRepository.save(Autor.fromDTO(autorDTO)));
+        Autor autor = Autor.fromDTO(autorDTO);
+        return new AutorDTO(autorRepository.save(autor));
     }
 
     public AutorDTO read(String id) {
-        Optional<Autor> optionalAutor = autorRepository.findById(id);
-        if (optionalAutor.isPresent()) {
-            return new AutorDTO(optionalAutor.get());
-        }
-        throw new NoSuchElementException("Autor com id " + id + " não encontrado");
+        Autor autor = findAutorById(id);
+        return new AutorDTO(autor);
     }
 
-    public AutorDTO update(String id, AutorDTO autorDto) {
-        Autor autor = Autor.fromDTO(autorDto);
-        autor.setId(id);
+    public AutorDTO update(String id, AutorDTO autorDTO) {
+        Autor autor = findAutorById(id); // Verifica se o autor existe antes de atualizar.
+        autor.fromDTO(autorDTO); // Metodo que atualiza os campos do autor.
         return new AutorDTO(autorRepository.save(autor));
     }
 
     public void delete(String id) {
+        findAutorById(id); // Garante que o autor existe antes de deletar.
         autorRepository.deleteById(id);
     }
 
@@ -43,4 +41,9 @@ public class AutorService {
         return autorRepository.findAll(pageable).map(AutorDTO::new);
     }
 
+    // Metodo utilitário privado para reduzir repetição de código.
+    private Autor findAutorById(String id) {
+        return autorRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Autor com ID '" + id + "' não encontrado"));
+    }
 }
